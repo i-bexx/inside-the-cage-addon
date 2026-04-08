25870a1ba16f858743c6801081c82b593e222798
i-bexx <yigitkarabacak364@gmail.com>
2026-04-09 00:50:33 +0300

    update: stamina logic now works on stamina bar

--- behavior_pack/scripts/UI/fastUiTick.js
+++ behavior_pack/scripts/UI/fastUiTick.js
@@ -1,6 +1,7 @@
 import { world, system, EquipmentSlot } from "@minecraft/server";
 
 import { slowUiTick } from "./slowUiTick";
+import { getStaminaObjective, getStaminaLimitObjective, getObjectiveScore } from "../scoreboards";
 
 const playerCompassStates = new Map();
 
@@ -11,29 +12,44 @@ function fastUiTick() {
 
   for (const player of players) {
     let cursorState;
+
     const isShooting = player.getComponent("variant").value == 5;
     const compassState = compassString(player);
 
     if (isShooting) cursorState = shootingCursorString(player);
     else cursorState = cursorString(player);
 
+    // Check if player is holding the gun
     const equippable = player.getComponent("minecraft:equippable");
     const mainHandItem = equippable.getEquipment(EquipmentSlot.Mainhand);
     const isHoldingGun = mainHandItem?.typeId === "game:gun";
 
+    // ----- STAMINA INFO -----
+    const staminaObjective = getStaminaObjective();
+    const staminaLimitObjective = getStaminaLimitObjective();
+    const playerStamina = getObjectiveScore(staminaObjective, player.scoreboardIdentity) ?? 10;
+    const playerStaminaLimit = getObjectiveScore(staminaLimitObjective, player.scoreboardIdentity) ?? 10;
+
     let uiString = "";
+
     if (isHoldingGun) uiString = `${cursorState}`;
+    else uiString = "cursorState_zz";
 
     const shouldCompassShown = playerCompassStates.get(player.id) !== compassState || player.getDynamicProperty("compassShowing");
     
     if (shouldCompassShown) {
         player.setDynamicProperty("compassShowing", true);
         playerCompassStates.set(player.id, compassState);
-
         uiString += ` ${compassState}`;
+    } else {
+      uiString += " compass_zz";
     }
-    if (uiString.length > 0) player.onScreenDisplay.updateSubtitle(uiString);
-    else player.onScreenDisplay.updateSubtitle(" ");
+
+    if (playerStaminaLimit == 10) uiString += ` stamina_x${playerStamina}`;
+    else if (playerStaminaLimit == 20) uiString += ` stamina_y${playerStamina}`;
+
+
+    player.onScreenDisplay.updateSubtitle(uiString);
 
     if (player.hasTag("updateSanityUI")) { // When kit is used, sanity info updates right away
       slowUiTick();
