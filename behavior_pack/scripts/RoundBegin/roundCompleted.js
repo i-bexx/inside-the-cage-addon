47a8b8e1f7a681b0a23fa9d7e3dc843cb40d3b79
i-bexx <yigitkarabacak364@gmail.com>
2026-07-30 16:59:06 +0300

    update: added animation for cinematic curtain

--- behavior_pack/scripts/RoundBegin/roundCompleted.js
+++ behavior_pack/scripts/RoundBegin/roundCompleted.js
@@ -3,6 +3,7 @@ import { world } from "@minecraft/server";
 import { sleep } from "../utils";
 import { cameraDeactivated } from "../cameraUsage";
 import { resetPlayerDynamicPropertyData, clearPlayerMaps, stopFunctionsInMaps, commandsToResetPlayerData } from "../resetStats";
+import { resetFunctions, resetMaps, resetWorldDynamicPropertyData, resetEntitiesData } from "../resetStats";
 
 export async function roundCompleted() {
   const players = world.getPlayers({tags: ["in_game"]});
@@ -17,19 +18,32 @@ export async function roundCompleted() {
     clearPlayerMaps(player.id);
     resetPlayerDynamicPropertyData(player);
 	}
+  
+  resetFunctions();
+  resetMaps();
+  resetWorldDynamicPropertyData();
+  resetWorldDynamicPropertyData();
+
 	world.setDynamicProperty("roundCompleted", true);
 
   await sleep(40);
 
-  for (const player of players) {
-   player.teleport({x: -186, y: 53, z: -82});
-  }
+  players.forEach(player => {
+    if (player.isValid) player.teleport({x: -186, y: 53, z: -82});
+  });
 
-  await sleep(100);
+  await sleep(40);
 
-  for (const player of players) {
-    commandsToResetPlayerData(player);
-  }
+  world.setDynamicProperty("roundCompleted", false);
+  players.forEach(player => {
+    if (player.isValid) player.triggerEvent("round_completed_dialog_event");
+  });
+
+  await sleep(400);
+
+  players.forEach(player => {
+    if (player.isValid) commandsToResetPlayerData(player);
+  });
 
   world.getDimension("overworld").runCommand("scoreboard players set value game_started 0");
 }
